06e5a2f15d2a37eb0cc9f00a2d16bcc034db4cb2
Eric Phillips <github@nekvtcode.com>
2022-09-16 12:02:47 -0400

    Add WordPress blocks plugin package

---
+++ packages/alex-blocks/alex-blocks.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:       Alex Blocks
+ * Description:       Custom Gutenberg blocks for Alex sites.
+ * Version:           1.0.0
+ * Author:            Eric Phillips
+ * Text Domain:       alexblocks
+ */
+
+function alexblocks_register_block_types() {
+    $blocks = glob( plugin_dir_path( __FILE__ ) . 'build/**/block.json', GLOB_NOSORT );
+
+    foreach( $blocks as $block ) {
+        register_block_type( $block );
+    }
+}
