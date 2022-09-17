f44044a30321fecda1c8d863960e3e27d50706ba
Eric Phillips <github@nekvtcode.com>
2022-09-17 07:31:45 -0400

    Connect front end to WordPress CMS

---
+++ packages/nextjs-frontend/pages/[[...path]].tsx
@@ -0,0 +1,26 @@
+const Post = ({post}) => {
+    return <h1>Hello World.</h1>
+}
+
+const getStaticPaths = async () => {
+    /** TODO: actually, intelligently implement this method */
+    return {
+        paths: [],
+        fallback: true
+    }
+}
+
+const getStaticProps = async (ctx) => {
+    const path = ctx.params?.path.join('/') || '/'
+    const endpoint = 'wp-json/alex/v1/posts'
+    const url = new URL(endpoint, process.env.WP_URL)
+    url.searchParams.set('path', path)
+    const res = await fetch(url)
+    const post = await res.json()
+
+    return { props: { post } }
+}
+
+export { getStaticProps, getStaticPaths }
+
+export default Post
