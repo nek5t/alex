f0be796c7f6097128a9d402e4f3ceb6bd7e2e365
Eric Phillips <github@nekvtcode.com>
2022-09-17 08:59:07 -0400

    Parse block attribute from HTML content

--- packages/alex-headless/alex-headless.php
+++ packages/alex-headless/alex-headless.php
@@ -9,16 +9,25 @@
 
 namespace AlexHeadless\Endpoint;
 
+include_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+
+use Symfony\Component\CssSelector\CssSelectorConverter;
 use add_action;
 use apply_filters;
+use DOMDocument;
+use DOMXPath;
 use esc_html__;
 use rest_ensure_response;
+use WP_Block_Type_Registry;
 use WP_Error;
 use WP_REST_Response;
 
 class AlexHeadless_REST_Controller {
     public function __construct()
     {
+        $this->html = new DOMDocument();
+        $this->cssConverter = new CssSelectorConverter();
+        $this->registered_block_types = WP_Block_Type_Registry::get_instance()->get_all_registered();
         $this->namespace = '/alex/v1';
         $this->route = '/posts';
     }
@@ -44,9 +53,94 @@ class AlexHeadless_REST_Controller {
     public function get_posts( $request ) {
         $accepted_content_types = apply_filters( 'alexhless_content_types', array( 'post', 'page' ) );
         $post = get_page_by_path( $request['path'], OBJECT, $accepted_content_types );
+        $post->blocks = $this->prepare_blocks( $post );
         $response = new WP_REST_Response( $post );
 
         return rest_ensure_response( $response );
+    }
+
+    private function prepare_blocks( $post ) {
+        $blocks = array_filter( parse_blocks( $post->post_content ), function($b) { return ! empty( $b['blockName'] ); } );
+
+        foreach( $blocks as &$block ) {
+            $metadata = $this->registered_block_types[$block['blockName']];
+
+            foreach($metadata->attributes as $attr_name => $attr) {
+                if (!empty($attr['source'])) {
+                    $attr_value = call_user_func_array(
+                        array( $this, 'get_source_' . $attr['source'] ), 
+                        array( $block['innerHTML'], $attr )
+                    );
+
+                    $block['attrs'] = array_merge(
+                        array( $attr_name => $attr_value ),
+                        $block['attrs']
+                    );
+                }
+            }
+        }
+
+        return $blocks;
+    }
+
+    private function get_source_html($html, $attr) {
+        $xpath = new DOMXPath( $this->load_html($html) );
+        $selector = $this->cssConverter->toXPath($attr['selector']);
+        $nodeList = $xpath->query($selector);
+
+        return $nodeList->item(0)?->nodeValue;
+    }
+
+    private function get_source_attribute($html, $attr) {
+        $xpath = new DOMXPath( $this->load_html($html) );
+        $attribute = $attr['attribute'];
+        $selector = $this->cssConverter($attr['selector']);
+        $selector .= "/@$attribute";
+        $nodeList = $xpath->query($selector);
+
+        return $nodeList->item(0)?->nodeValue;
+    }
+
+    private function get_source_text($html, $attr) {
+        $xpath = new DOMXPath( $this->load_html($html) );
+        $selector = $this->cssConverter->toXPath($attr['selector']);
+        $nodeList = $xpath->query($selector);
+
+        return $nodeList->item(0)?->textContent;
+    }
+
+    private function get_source_query($html, $attr) {
+        $xpath = new DOMXPath( $this->load_html($html) );
+        $selector = $this->cssConverter->toXPath($attr['selector']);
+        $nodeList = $this->query($selector);
+        $result = array();
+
+        foreach($nodeList as $node) {
+            $node_html = $this->save_html($node);
+
+            foreach($attr['query'] as $key => $query) {
+                $query_result = call_user_func_array(
+                    array( $this, 'get_source_' . $query['source'] ),
+                    array( $node_html, $query )
+                );
+                array_merge(
+                    array( $key => $query_result ),
+                    $result
+                );
+            }
+        }
+
+        return $result;
+    }
+
+    private function save_html($node) {
+        return $this->html->saveHTML($node);
+    }
+
+    private function load_html($html) {
+        $this->html->loadHtml($html, LIBXML_HTML_NODEFDTD | LIBXML_HTML_NOIMPLIED);
+
+        return $this->html;
     }
 }
 
