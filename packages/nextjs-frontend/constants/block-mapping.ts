5ea52134166b30b10f0999f386f0c9768f5ca1cd
Eric Phillips <github@nekvtcode.com>
2022-09-18 13:55:32 -0400

    Add frontend support for Details block

--- packages/nextjs-frontend/constants/block-mapping.ts
+++ packages/nextjs-frontend/constants/block-mapping.ts
@@ -1,8 +1,9 @@
-import { Paragraph, Quote } from '@alex/components'
+import { Paragraph, Quote, Details } from '@alex/components'
 
 const blockMap = new Map();
 
 blockMap.set('core/paragraph', Paragraph)
 blockMap.set('core/quote', Quote)
+blockMap.set('alexblocks/details', Details)
 
 export default blockMap
