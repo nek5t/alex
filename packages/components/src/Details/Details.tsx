3209322a4bc3dbaae19946a9d55dded3ec8e8103
Eric Phillips <github@nekvtcode.com>
2022-09-19 05:38:14 -0400

    Update Detail component

--- packages/components/src/Details/Details.tsx
+++ packages/components/src/Details/Details.tsx
@@ -1,26 +1,28 @@
-import React, { ReactElement } from "react"
+import React, { ReactElement } from 'react';
 
-import { InputTemplate } from '../../lib'
-import styles from './Details.module.css'
+import { InputTemplate } from '../../lib';
+import { details as styles } from './Details.module.css';
 
 export interface DetailsProps {
-    summary: ReactElement | string[],
-    details: ReactElement | ReactElement[],
-    blockProps?: Record<string, any>
+	summary: ReactElement | string[];
+	details: ReactElement | ReactElement[];
+	blockProps?: Record<string, any>;
 }
 
-const Details = ({summary, details, ...props} : DetailsProps) => {
-    const {innerBlocks, ...blockProps} = props
-    const detailsContent = details || innerBlocks
-    const summaryTemplate = () => <h3>{summary}</h3>
+const Details = ({ summary, details, ...props }: DetailsProps) => {
+	const { innerBlocks, ...blockProps } = props;
+	const detailsContent = details || innerBlocks;
+	const summaryTemplate = () => <h3>{summary}</h3>;
 
-    return (
-        <details {...blockProps} className={styles.details}>
-            <summary><InputTemplate template={summaryTemplate} input={summary} /></summary>
+	return (
+		<details {...blockProps} className={styles}>
+			<summary>
+				<InputTemplate template={summaryTemplate} input={summary} />
+			</summary>
 
-            {detailsContent}
-        </details>
-    ) 
-}
+			{detailsContent}
+		</details>
+	);
+};
 
-export default Details
+export default Details;
