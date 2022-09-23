9c4a1d1dc2644ddb7916e35d2c0c9d99a8ec0e19
Eric Phillips <github@nekvtcode.com>
2022-09-23 09:45:29 -0400

    Add module CSS restricting paragraph width

--- packages/components/src/Paragraph/Paragraph.tsx
+++ packages/components/src/Paragraph/Paragraph.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { GutenbergBlock } from '../../lib';
+import styles from './Paragraph.module.css';
 
 export interface ParagraphProps extends GutenbergBlock {
 	content: string;
 }
 
 const Paragraph = ({ content: __html, props }: ParagraphProps) => {
-	return <p {...props} dangerouslySetInnerHTML={{ __html }} />;
+	return (
+		<p
+			{...props}
+			dangerouslySetInnerHTML={{ __html }}
+			className={styles.paragraph}
+		/>
+	);
 };
 
 export default Paragraph;
