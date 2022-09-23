51a9d4328ceded7d8c8691739d85be89b84738e8
Eric Phillips <github@nekvtcode.com>
2022-09-23 03:58:33 -0400

    Use MDX for component stories/docs

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -1,17 +1,17 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "storybook-css-modules"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": 'webpack5',
-    "disableTelemetry": true,
-  },
-}
+	stories: [
+		'../src/**/*.stories.mdx',
+		'../src/**/*.stories.@(js|jsx|ts|tsx)',
+	],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'storybook-css-modules',
+	],
+	framework: '@storybook/react',
+	core: {
+		builder: 'webpack5',
+		disableTelemetry: true,
+	},
+};
