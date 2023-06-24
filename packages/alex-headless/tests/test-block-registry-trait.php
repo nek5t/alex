5df6c71a3ab1388398cee9b073cde7666da64d3d
Eric Phillips <github@nekvtcode.com>
2023-06-24 03:24:33 -0400

    Test that registered blocks are returned

--- packages/alex-headless/tests/test-block-registry-trait.php
+++ packages/alex-headless/tests/test-block-registry-trait.php
@@ -11,21 +11,36 @@ use Alex\Headless\Parser\UnregisteredBlockException;
 
 class UsesBlockRegistryTraitTest extends WP_UnitTestCase
 {
-	public function test_exception_for_unregistered_blocks()
-	{
-		$this->expectException(UnregisteredBlockException::class);
+	protected $mock;
 
+	public function setUp() : void
+	{
 		/**
-		 * Create mock object with protected method
-		 * set to public visibility.
+		 * Create mock object that uses our trait,
+		 * with visibility of the method we would
+		 * like to test set to public.
 		 */
-		$mock = new class
+		$this->mock = new class
 		{
 			use usesBlockRegistry {
 				getBlockType as public;
 			}
 		};
+	}
 
-		$mock->getBlockType(array('blockName' => 'unregistered/block-type'));
+	public function test_exception_for_unregistered_blocks()
+	{
+		$this->expectException(UnregisteredBlockException::class);
+
+		$this->mock->getBlockType(array('blockName' => 'unregistered/block-type'));
+	}
+
+	public function test_returns_registered_block()
+	{
+		$block = $this->mock->getBlockType(array(
+			'blockName' => 'core/button'
+		));
+
+		$this->assertInstanceOf(WP_Block_Type::class, $block);
 	}
 }
